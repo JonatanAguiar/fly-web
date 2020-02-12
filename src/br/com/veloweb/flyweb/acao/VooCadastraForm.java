e9d060cba3b9fda90f7627bd93aa332656564cca
Caroline Rosa <cpauladarosa@gmail.com>
2020-02-12 16:25:36 -0300

    voo cadastra form

--- src/br/com/veloweb/flyweb/acao/VooCadastraForm.java
+++ src/br/com/veloweb/flyweb/acao/VooCadastraForm.java
@@ -4,12 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.veloweb.flyweb.modelo.Banco;
+import br.com.veloweb.flyweb.modelo.Rota;
 
 public class VooCadastraForm implements Acao{
 
 	@Override
 	public String executa(HttpServletRequest request, HttpServletResponse response) {
 		Banco banco = new Banco();
+		List<Rota> listaDeRotas = banco.getRotas();
+		request.setAttribute("listaDeRotas",listaDeRotas);
 		return "fw:formCadastroVoo.jsp";
 	}
 
